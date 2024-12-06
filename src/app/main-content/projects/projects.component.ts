a1281a9233ce07e8ac5ba5c6f5357819427956c7
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-12-06 12:51:09 +0100

    project list

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -5,8 +5,40 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
-
+  projects: {
+    projectName: string;
+    languages: string[];
+    description: string;
+    links: {
+      github: string;
+      live: string;
+    };
+    screenshot: string;
+  }[] = [
+    {
+      projectName: 'Join',
+      languages: ['JavaScript', 'HTML', 'CSS', 'Firebase'],
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      links: {
+        github: '',
+        live: '',
+      },
+      screenshot: 'project-join.png',
+    },
+    {
+      projectName: 'El Pollo Loco',
+      languages: ['JavaScript', 'HTML', 'CSS'],
+      description:
+        'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      links: {
+        github: '',
+        live: '',
+      },
+      screenshot: 'project-epl.png',
+    },
+  ];
 }
