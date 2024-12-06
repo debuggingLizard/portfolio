eebd25ddd74df4643435b1dde4f4c1dc88249b2f
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-12-06 12:13:02 +0100

    skill-list

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -5,8 +5,48 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
-
+  skills: {
+    skillName: string;
+    skillIcon: string;
+  }[] = [
+    {
+      skillName: 'Angular',
+      skillIcon: 'icon-angular.svg',
+    },
+    {
+      skillName: 'TypeScript',
+      skillIcon: 'icon-typescript.svg',
+    },
+    {
+      skillName: 'JavaScript',
+      skillIcon: 'icon-javascript.svg',
+    },
+    {
+      skillName: 'HTML',
+      skillIcon: 'icon-html.svg',
+    },
+    {
+      skillName: 'CSS',
+      skillIcon: 'icon-css.svg',
+    },
+    {
+      skillName: 'Rest-Api',
+      skillIcon: 'icon-restapi.svg',
+    },
+    {
+      skillName: 'Firebase',
+      skillIcon: 'icon-firebase.svg',
+    },
+    {
+      skillName: 'GIT',
+      skillIcon: 'icon-git.svg',
+    },
+    {
+      skillName: 'Scrum',
+      skillIcon: 'icon-scrum.svg',
+    },
+  ];
 }
