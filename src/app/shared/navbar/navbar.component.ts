a8700ef93e803704552ae0a7337a361300aee132
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-12-13 04:35:35 +0100

    started working on navbar responsive

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -2,14 +2,21 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../../translation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, TranslateModule],
+  imports: [RouterModule, TranslateModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   translate = inject(TranslationService);
+
+  menuToggle:string = "";
+
+  toggleMenu() { 
+    this.menuToggle = this.menuToggle == "" ? "open" : "";
+  }
 }
